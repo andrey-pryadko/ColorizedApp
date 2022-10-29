ab181581bf1f873d1691abe1d30085802efa50bd
Andrey Pryadko <andrey@pryadko.com>
2022-10-29 10:33:28 +0300

    color changing of main view added

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -9,11 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+// MARK: - IB Outlets
+    @IBOutlet var mainView: UIView!
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        reloadColorsOfMainView()
     }
 
+// MARK: IB Actions
+    @IBAction func redSliderAction() {
+        redLabel.text = String(format: "%.2f", redSlider.value)
+        reloadColorsOfMainView()
+    }
+    
+    @IBAction func greenSliderAction() {
+        greenLabel.text = String(format: "%.2f", greenSlider.value)
+        reloadColorsOfMainView()
+    }
+    
+    @IBAction func blueSliderAction() {
+        blueLabel.text = String(format: "%.2f", blueSlider.value)
+        reloadColorsOfMainView()
+    }
 
+// MARK: Private Methods
+    private func reloadColorsOfMainView() {
+        mainView.backgroundColor = UIColor(displayP3Red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+    }
+
+    
 }
+
 
